7aa6a423a4b782282805a3307223e9bc4e7190f2
thgomes3 <thiagovg9@icloud.com>
2025-06-05 16:02:36 -0300

    Create SomaTresNumeros.java

---
+++ SomaTresNumeros.java
@@ -0,0 +1,10 @@
+public class SomaTresNumeros {
+    public static int soma(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    public static void main(String[] args) {
+        int resultado = soma(5, 10, 3);
+        System.out.println("A soma dos três números é: " + resultado);
+    }
+}
